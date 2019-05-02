d63a5c0c7ebe526fb0d01903d9e67fe06b6678d0
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-05-02 15:21:59 +0100

    Added IntegrationTest

---
+++ src/test/java/uk.gov.ons.census.action/messaging/EventReceiverIT.java
@@ -0,0 +1,4 @@
+package uk.gov.ons.census.action.messaging;
+
+public class EventReceiverIT {
+}
---
+++ src/test/java/uk.gov.ons.census.action/messaging/RabbitQueueHelper.java
@@ -0,0 +1,59 @@
+package testutil;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+@Component
+@EnableRetry
+public class RabbitQueueHelper {
+
+  @Autowired private ConnectionFactory connectionFactory;
+
+  @Autowired private RabbitTemplate rabbitTemplate;
+
+  @Autowired private AmqpAdmin amqpAdmin;
+
+  public BlockingQueue<String> listen(String queueName) {
+    BlockingQueue<String> transfer = new ArrayBlockingQueue(50);
+
+    org.springframework.amqp.core.MessageListener messageListener =
+        message -> {
+          String msgStr = new String(message.getBody());
+          transfer.add(msgStr);
+        };
+    SimpleMessageListenerContainer container =
+        new SimpleMessageListenerContainer(connectionFactory);
+    container.setMessageListener(messageListener);
+    container.setQueueNames(queueName);
+    container.start();
+
+    return transfer;
+  }
+
+  @Retryable(
+      value = {java.io.IOException.class},
+      maxAttempts = 10,
+      backoff = @Backoff(delay = 5000))
+  public void sendMessage(String queueName, Object message) {
+    rabbitTemplate.convertAndSend(queueName, message);
+  }
+
+  @Retryable(
+      value = {java.io.IOException.class},
+      maxAttempts = 10,
+      backoff = @Backoff(delay = 5000))
+  public void purgeQueue(String queueName) {
+    amqpAdmin.purgeQueue(queueName);
+  }
+}
