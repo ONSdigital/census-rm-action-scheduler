9afd5f45bf56e91cedf8ab583e168a673db2f6f8
Richard Weeks <richard.weeks@qa.com>
2019-05-29 13:13:10 +0100

    Apply reformat maven plugin.

--- src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
+++ src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
@@ -1,7 +1,6 @@
 package uk.gov.ons.census.action.model.entity;
 
 public enum ActionType {
-
   ICL1E("Printer"), // Census initial contact letter for England
   ICL2W("Printer"), // Census initial contact letter for Wales
   ICHHQW("Printer"); // Census household questionnaire for Wales
