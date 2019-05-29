280d04051409135043751f779004a5b4e9d45dcb
Richard Weeks <richard.weeks@qa.com>
2019-05-29 08:21:06 +0100

    Change Wales ICL action type code.

--- src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
+++ src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
@@ -1,8 +1,9 @@
 package uk.gov.ons.census.action.model.entity;
 
 public enum ActionType {
+
   ICL1E("Printer"), // Census initial contact letter for England
-  ICL2E("Printer"), // Census initial contact letter for Wales
+  ICL2W("Printer"), // Census initial contact letter for Wales
   ICHHQW("Printer"); // Census household questionnaire for Wales
 
   private final String handler;
