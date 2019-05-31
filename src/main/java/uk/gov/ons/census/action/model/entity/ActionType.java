3a0316f6c68d141a86039e5f5c0ed9ece598b958
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-05-31 15:41:05 +0100

    chnages for NI Initial

--- src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
+++ src/main/java/uk/gov/ons/census/action/model/entity/ActionType.java
@@ -1,9 +1,13 @@
 package uk.gov.ons.census.action.model.entity;
 
 public enum ActionType {
-  ICL1E("Printer"), // Census initial contact letter for England
-  ICL2W("Printer"), // Census initial contact letter for Wales
-  ICHHQW("Printer"); // Census household questionnaire for Wales
+  ICL1E(Constants.PRINTER), // Census initial contact letter for England
+  ICL2W(Constants.PRINTER), // Census initial contact letter for Wales
+  ICL4E(Constants.PRINTER), // Census intiial contact letter for NI
+
+  ICHHQE(Constants.PRINTER), // Census household questionnaire for England
+  ICHHQW(Constants.PRINTER), // Census household questionnaire for Wales
+  ICHHQN(Constants.PRINTER); // Census household questionnaire for NI
 
   private final String handler;
 
@@ -13,5 +17,9 @@ public enum ActionType {
 
   public String getHandler() {
     return handler;
+  }
+
+  private static class Constants {
+    public static final String PRINTER = "Printer";
   }
 }
