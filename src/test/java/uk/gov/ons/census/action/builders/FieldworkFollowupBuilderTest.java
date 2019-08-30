aa171806ba110d8ac6344e7fdf6df2d49afeb18a
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-08-30 11:15:35 +0100

    Do the things

--- src/test/java/uk/gov/ons/census/action/builders/FieldworkFollowupBuilderTest.java
+++ src/test/java/uk/gov/ons/census/action/builders/FieldworkFollowupBuilderTest.java
@@ -46,7 +46,7 @@ public class FieldworkFollowupBuilderTest {
 
     // Then
     assertThat(actualResult.getSurveyName()).isEqualTo("CENSUS");
-    assertThat(actualResult.getUndeliveredAsAddress()).isFalse();
+    assertThat(actualResult.getUndeliveredAsAddress()).isEqualTo(caze.isUndeliveredAsAddressed());
     assertThat(actualResult.getBlankQreReturned()).isFalse();
     assertThat(actualResult.getCaseId()).isEqualTo(caze.getCaseId().toString());
     assertThat(actualResult.getCaseRef()).isEqualTo(Integer.toString(caze.getCaseRef()));
