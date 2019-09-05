f914f3cd1eff471a50e366b456fff264094baace
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2019-09-05 08:42:51 +0100

    Rmc 211 individual response mappings (#35)
    * add mappings for Indiividual Print Questionaires and link their Qids to IndividualCaseId
    
    * map Individual Print Questionnaire requests
    
    * clean up here be dragons
    
    * removed unused enum
    
    * correct mappings
    
    * remove unrequired enum work
    
    * actiontypes for individual print questionnaires reduced to P_OR_IX for all
    
    * remove unused comment
    
    * fix potential injection issue by using UriComponents

--- src/test/java/uk.gov.ons.census.action/messaging/FulfilmentRequestReceiverTest.java
+++ src/test/java/uk.gov.ons.census.action/messaging/FulfilmentRequestReceiverTest.java
@@ -4,9 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -20,6 +18,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import uk.gov.ons.census.action.client.CaseClient;
+import uk.gov.ons.census.action.model.dto.FulfilmentRequestDTO;
 import uk.gov.ons.census.action.model.dto.PrintFileDto;
 import uk.gov.ons.census.action.model.dto.ResponseManagementEvent;
 import uk.gov.ons.census.action.model.dto.UacQidDTO;
@@ -29,6 +28,11 @@ import uk.gov.ons.census.action.model.repository.CaseRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FulfilmentRequestReceiverTest {
+  private static final String PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_ENGLAND = "P_OR_I1";
+  private static final String PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_WALES_ENGLISH = "P_OR_I2";
+  private static final String PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_WALES_WELSH = "P_OR_I2W";
+  private static final String PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_NORTHERN_IRELAND = "P_OR_I4";
+  private static UUID TEST_INDIVIDUAL_CASE_ID = UUID.randomUUID();
 
   @Mock private RabbitTemplate rabbitTemplate;
   @Mock private CaseClient caseClient;
@@ -53,6 +57,9 @@ public class FulfilmentRequestReceiverTest {
     // When
     underTest.receiveEvent(event);
 
+    verifyZeroInteractions(caseRepository);
+    verifyZeroInteractions(rabbitTemplate);
+    verifyZeroInteractions(caseClient);
     // Then no exception is thrown
   }
 
@@ -66,6 +73,9 @@ public class FulfilmentRequestReceiverTest {
     // When
     underTest.receiveEvent(event);
 
+    verifyZeroInteractions(caseRepository);
+    verifyZeroInteractions(rabbitTemplate);
+    verifyZeroInteractions(caseClient);
     // Then no exception is thrown
   }
 
@@ -75,6 +85,7 @@ public class FulfilmentRequestReceiverTest {
     Case fulfilmentCase = caseRepositoryReturnsRandomCase();
     ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
     event.getPayload().getFulfilmentRequest().setFulfilmentCode("P_OR_H1");
+    event.getPayload().getFulfilmentRequest().setCaseId(fulfilmentCase.getCaseId());
     UacQidDTO uacQidDTO = easyRandom.nextObject(UacQidDTO.class);
     when(caseClient.getUacQid(eq(fulfilmentCase.getCaseId()), eq("1"))).thenReturn(uacQidDTO);
 
@@ -93,8 +104,9 @@ public class FulfilmentRequestReceiverTest {
     Case fulfilmentCase = caseRepositoryReturnsRandomCase();
     ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
     event.getPayload().getFulfilmentRequest().setFulfilmentCode("P_OR_HC1");
+    event.getPayload().getFulfilmentRequest().setCaseId(fulfilmentCase.getCaseId());
     UacQidDTO uacQidDTO = easyRandom.nextObject(UacQidDTO.class);
-    when(caseClient.getUacQid(eq(fulfilmentCase.getCaseId()), eq("11"))).thenReturn(uacQidDTO);
+    when(caseClient.getUacQid(fulfilmentCase.getCaseId(), "11")).thenReturn(uacQidDTO);
 
     // When
     underTest.receiveEvent(event);
@@ -106,11 +118,85 @@ public class FulfilmentRequestReceiverTest {
   }
 
   @Test
+  public void testOnRequestIndividualQuestionnaireFulfilmentEngland() {
+    testIndividualResponseRequest(
+        PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_ENGLAND, "21", ActionType.P_OR_IX);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testRequestIndividualQuestionaireWithMissingCaseThrowsExceptions() {
+    Case individualFulfilmentCase = easyRandom.nextObject(Case.class);
+    individualFulfilmentCase.setCaseId(TEST_INDIVIDUAL_CASE_ID);
+
+    individualFulfilmentCase.setCaseId(TEST_INDIVIDUAL_CASE_ID);
+    ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
+    FulfilmentRequestDTO fulfilmentRequest = event.getPayload().getFulfilmentRequest();
+    fulfilmentRequest.setFulfilmentCode(PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_ENGLAND);
+    fulfilmentRequest.setIndividualCaseId(TEST_INDIVIDUAL_CASE_ID);
+
+    // When
+    underTest.receiveEvent(event);
+
+    verifyZeroInteractions(caseClient);
+    verifyZeroInteractions(rabbitTemplate);
+  }
+
+  @Test
+  public void testOnRequestIndividualQuestionnaireFulfilmentWalesEnglish() {
+    testIndividualResponseRequest(
+        PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_WALES_ENGLISH, "22", ActionType.P_OR_IX);
+  }
+
+  @Test
+  public void testOnRequestIndividualQuestionnaireFulfilmentWalesWelsh() {
+    testIndividualResponseRequest(
+        PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_WALES_WELSH, "23", ActionType.P_OR_IX);
+  }
+
+  @Test
+  public void testOnRequestIndividualQuestionnaireFulfilmentNorthernIreland() {
+    testIndividualResponseRequest(
+        PRINT_INDIVIDUAL_QUESTIONNAIRE_REQUEST_NORTHERN_IRELAND, "24", ActionType.P_OR_IX);
+  }
+
+  private void testIndividualResponseRequest(
+      String fulfilmentCode, String expectedQuestionaireType, ActionType expectedActionType) {
+
+    // Given
+    Case individualFulfilmentCase = easyRandom.nextObject(Case.class);
+    individualFulfilmentCase.setCaseId(TEST_INDIVIDUAL_CASE_ID);
+
+    when(caseRepository.findByCaseId(TEST_INDIVIDUAL_CASE_ID))
+        .thenReturn(Optional.of(individualFulfilmentCase));
+
+    individualFulfilmentCase.setCaseId(TEST_INDIVIDUAL_CASE_ID);
+    ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
+    FulfilmentRequestDTO fulfilmentRequest = event.getPayload().getFulfilmentRequest();
+    fulfilmentRequest.setFulfilmentCode(fulfilmentCode);
+    fulfilmentRequest.setIndividualCaseId(TEST_INDIVIDUAL_CASE_ID);
+
+    UacQidDTO uacQidDTO = easyRandom.nextObject(UacQidDTO.class);
+
+    when(caseClient.getUacQid(TEST_INDIVIDUAL_CASE_ID, expectedQuestionaireType))
+        .thenReturn(uacQidDTO);
+
+    // When
+    underTest.receiveEvent(event);
+
+    // Then
+    PrintFileDto actualPrintFileDTO =
+        checkCorrectPackCodeAndAddressAreSent(event, individualFulfilmentCase, expectedActionType);
+
+    assertThat(actualPrintFileDTO).isEqualToComparingOnlyGivenFields(uacQidDTO, "uac", "qid");
+  }
+
+  @Test
   public void testLargePrintQuestionnaireFulfilment() {
     // Given
     Case fulfilmentCase = caseRepositoryReturnsRandomCase();
     ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
     event.getPayload().getFulfilmentRequest().setFulfilmentCode("P_LP_HL1");
+    event.getPayload().getFulfilmentRequest().setCaseId(fulfilmentCase.getCaseId());
 
     // When
     underTest.receiveEvent(event);
@@ -126,6 +212,7 @@ public class FulfilmentRequestReceiverTest {
     Case fulfilmentCase = caseRepositoryReturnsRandomCase();
     ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
     event.getPayload().getFulfilmentRequest().setFulfilmentCode("P_TB_TBARA1");
+    event.getPayload().getFulfilmentRequest().setCaseId(fulfilmentCase.getCaseId());
 
     // When
     underTest.receiveEvent(event);
@@ -160,7 +247,7 @@ public class FulfilmentRequestReceiverTest {
 
   private Case caseRepositoryReturnsRandomCase() {
     Case caze = easyRandom.nextObject(Case.class);
-    when(caseRepository.findByCaseId(any(UUID.class))).thenReturn(Optional.of(caze));
+    when(caseRepository.findByCaseId(caze.getCaseId())).thenReturn(Optional.of(caze));
     return caze;
   }
 }
