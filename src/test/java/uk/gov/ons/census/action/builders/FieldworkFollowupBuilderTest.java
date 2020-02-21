c80a07b477671e7be30982f6500a5301c4047019
liamtoozer <liam.toozer@gmail.com>
2020-02-21 12:48:12 +0000

    Add check to test

--- src/test/java/uk/gov/ons/census/action/builders/FieldworkFollowupBuilderTest.java
+++ src/test/java/uk/gov/ons/census/action/builders/FieldworkFollowupBuilderTest.java
@@ -53,6 +53,7 @@ public class FieldworkFollowupBuilderTest {
     assertThat(actualResult.getBlankQreReturned()).isFalse();
     assertThat(actualResult.getCaseId()).isEqualTo(caze.getCaseId().toString());
     assertThat(actualResult.getCaseRef()).isEqualTo(Integer.toString(caze.getCaseRef()));
+    assertThat(actualResult.isHandDelivery()).isEqualTo(caze.isHandDelivery());
     assertThat(actualResult)
         .isEqualToIgnoringGivenFields(
             caze,
