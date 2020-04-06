ebbc9ea8bb3e4d6f27232e1a484e35b995fef8d9
Adam <35268982+AdamHawtin@users.noreply.github.com>
2020-04-06 11:43:11 +0100

    AM1 Part 4 New Address Fulfilments (#74)

--- src/main/java/uk/gov/ons/census/action/service/FulfilmentRequestService.java
+++ src/main/java/uk/gov/ons/census/action/service/FulfilmentRequestService.java
@@ -2,8 +2,12 @@ package uk.gov.ons.census.action.service;
 
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,6 +30,20 @@ public class FulfilmentRequestService {
   private final CaseClient caseClient;
   private final CaseSelectedBuilder caseSelectedBuilder;
   private final FulfilmentToSendRepository fulfilmentToSendRepository;
+  private static final Set<String> paperQuestionnaireFulfilmentCodes =
+      Set.of(
+          "P_OR_H1",
+          "P_OR_H2",
+          "P_OR_H2W",
+          "P_OR_H4",
+          "P_OR_HC1",
+          "P_OR_HC2",
+          "P_OR_HC2W",
+          "P_OR_HC4",
+          "P_OR_I1",
+          "P_OR_I2",
+          "P_OR_I2W",
+          "P_OR_I4");
 
   @Value("${queueconfig.outbound-exchange}")
   private String outboundExchange;
@@ -47,6 +65,8 @@ public class FulfilmentRequestService {
   public void processEvent(
       FulfilmentRequestDTO fulfilmentRequest, Case caze, ActionType actionType) {
 
+    checkMandatoryFields(fulfilmentRequest, caze);
+
     PrintFileDto printFileDto = createAndPopulatePrintFileDto(caze, actionType, fulfilmentRequest);
 
     ResponseManagementEvent printCaseSelected =
@@ -55,6 +75,40 @@ public class FulfilmentRequestService {
     rabbitTemplate.convertAndSend(actionCaseExchange, "", printCaseSelected);
 
     sendFulfilmentToTable(printFileDto, fulfilmentRequest);
+  }
+
+  private static void checkMandatoryFields(FulfilmentRequestDTO fulfilmentRequest, Case caze) {
+    /*
+    Throws a RuntimeException if the case does not have the minimum data according to the mandatory fields listed here
+    https://collaborate2.ons.gov.uk/confluence/display/SDC/Handle+New+Address+Reported+Events
+     */
+    Map<String, Object> mandatoryValues = new HashMap<>();
+    mandatoryValues.put("addressLine1", caze.getAddressLine1());
+    mandatoryValues.put("postcode", caze.getPostcode());
+    mandatoryValues.put("townName", caze.getTownName());
+
+    if (!isPaperQuestionnaireFulfilment(fulfilmentRequest) && caze.isHandDelivery()) {
+      // Non PQ fulfilments which are hand delivered need a field officer and coordinator
+      mandatoryValues.put("fieldCoordinatorId", caze.getFieldCoordinatorId());
+      mandatoryValues.put("fieldOfficerId", caze.getFieldOfficerId());
+    }
+
+    List<String> missingFields = new ArrayList<>();
+    for (Map.Entry<String, Object> entry : mandatoryValues.entrySet()) {
+      if (entry.getValue() == null) {
+        missingFields.add(entry.getKey());
+      }
+    }
+    if (!missingFields.isEmpty()) {
+      throw new RuntimeException(
+          String.format(
+              "Received fulfilment request for case which is missing mandatory values: %s, fulfilmentCode: %s, caseId: %s",
+              missingFields.toString(), fulfilmentRequest.getFulfilmentCode(), caze.getCaseId()));
+    }
+  }
+
+  private static boolean isPaperQuestionnaireFulfilment(FulfilmentRequestDTO fulfilmentRequest) {
+    return paperQuestionnaireFulfilmentCodes.contains(fulfilmentRequest.getFulfilmentCode());
   }
 
   public void sendFulfilmentToTable(
