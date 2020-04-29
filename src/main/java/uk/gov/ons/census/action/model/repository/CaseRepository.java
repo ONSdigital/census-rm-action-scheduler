75aae47e03b95c1bb033384e945ad75ca711243f
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2020-04-29 14:37:49 +0100

    Generate CE Estab Addressed ICL Print Files (#78)
    * Attempt to get sum of expected_capacity
    
    * Method to distinguish Estab cases and test for it
    
    * Integration test added
    
    Co-authored-by: Ryan Grundy <ryan.grundy@ons.gov.uk>

--- src/main/java/uk/gov/ons/census/action/model/repository/CaseRepository.java
+++ src/main/java/uk/gov/ons/census/action/model/repository/CaseRepository.java
@@ -1,7 +1,6 @@
 package uk.gov.ons.census.action.model.repository;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RestResource;
--- src/test/java/uk/gov/ons/census/action/schedule/CaseClassifierTest.java
+++ src/test/java/uk/gov/ons/census/action/schedule/CaseClassifierTest.java
@@ -13,12 +13,14 @@ import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import uk.gov.ons.census.action.model.entity.ActionPlan;
 import uk.gov.ons.census.action.model.entity.ActionRule;
+import uk.gov.ons.census.action.model.entity.ActionType;
 
 public class CaseClassifierTest {
   @Test
   public void testEnqueueCasesForActionRule() {
     // Given
     JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+
     CaseClassifier underTest = new CaseClassifier(jdbcTemplate);
     Map<String, List<String>> classifiers = new HashMap<>();
     classifiers.put("treatment_code", List.of("abc", "xyz"));
@@ -28,6 +30,7 @@ public class CaseClassifierTest {
     actionRule.setId(UUID.randomUUID());
     actionRule.setActionPlan(actionPlan);
     actionRule.setClassifiers(classifiers);
+    actionRule.setActionType(ActionType.CE1_IC01);
 
     // When
     underTest.enqueueCasesForActionRule(actionRule);
@@ -41,6 +44,39 @@ public class CaseClassifierTest {
     expectedSql.append("' AND receipt_received='f' AND refusal_received='f' AND ");
     expectedSql.append("address_invalid='f' AND case_type != 'HI' AND treatment_code IN ");
     expectedSql.append("('abc','xyz')");
+    verify(jdbcTemplate)
+        .update(eq(expectedSql.toString()), any(UUID.class), eq(actionRule.getId()));
+  }
+
+  @Test
+  public void testEnqueueCeEstabCasesForActionRule() {
+    // Given
+    JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+
+    CaseClassifier underTest = new CaseClassifier(jdbcTemplate);
+    Map<String, List<String>> classifiers = new HashMap<>();
+    classifiers.put("treatment_code", List.of("abc", "xyz"));
+    ActionPlan actionPlan = new ActionPlan();
+    actionPlan.setId(UUID.randomUUID());
+    ActionRule actionRule = new ActionRule();
+    actionRule.setId(UUID.randomUUID());
+    actionRule.setActionPlan(actionPlan);
+    actionRule.setClassifiers(classifiers);
+    actionRule.setActionType(ActionType.CE_IC03);
+
+    // When
+    underTest.enqueueCasesForActionRule(actionRule);
+
+    // Then
+    StringBuilder expectedSql = new StringBuilder();
+    expectedSql.append("INSERT INTO actionv2.case_to_process (batch_id, batch_quantity, ");
+    expectedSql.append("action_rule_id, caze_case_ref, ce_expected_capacity) SELECT ?, ");
+    expectedSql.append("SUM(ce_expected_capacity) OVER(), ?, case_ref, ce_expected_capacity ");
+    expectedSql.append("FROM actionv2.cases WHERE action_plan_id='");
+    expectedSql.append(actionPlan.getId().toString());
+    expectedSql.append("' AND receipt_received='f' AND refusal_received='f' AND ");
+    expectedSql.append("address_invalid='f' AND case_type != 'HI' AND treatment_code IN ");
+    expectedSql.append("('abc','xyz') GROUP BY case_ref");
     verify(jdbcTemplate)
         .update(eq(expectedSql.toString()), any(UUID.class), eq(actionRule.getId()));
   }
