4ccdeb8a298140297123f074d1a8e795848aae3c
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-06-01 10:53:28 +0100

    Fix fulfilment batch race condition

--- src/main/java/uk/gov/ons/census/action/model/repository/FulfilmentToSendRepository.java
+++ src/main/java/uk/gov/ons/census/action/model/repository/FulfilmentToSendRepository.java
@@ -4,15 +4,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RestResource;
-import uk.gov.ons.census.action.model.entity.FulfilmentCodeCount;
 import uk.gov.ons.census.action.model.entity.FulfilmentToSend;
 
 @RestResource(exported = false)
-public interface FulfilmentToSendRepository extends JpaRepository<FulfilmentToSend, String> {
+public interface FulfilmentToSendRepository extends JpaRepository<FulfilmentToSend, Long> {
 
-  @Query(
-      value =
-          "SELECT new uk.gov.ons.census.action.model.entity.FulfilmentCodeCount(f.fulfilmentCode, count(f)) "
-              + "from FulfilmentToSend f group by f.fulfilmentCode ")
-  List<FulfilmentCodeCount> findCountOfFulfilments();
+  @Query("SELECT DISTINCT f.fulfilmentCode FROM FulfilmentToSend f")
+  List<String> findDistinctFulfilmentCode();
 }
--- src/main/java/uk/gov/ons/census/action/schedule/FulfilmentProcessor.java
+++ src/main/java/uk/gov/ons/census/action/schedule/FulfilmentProcessor.java
@@ -2,9 +2,9 @@ package uk.gov.ons.census.action.schedule;
 
 import java.util.List;
 import java.util.UUID;
+import javax.transaction.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.action.model.entity.FulfilmentCodeCount;
 import uk.gov.ons.census.action.model.repository.FulfilmentToSendRepository;
 
 @Component
@@ -19,18 +19,19 @@ public class FulfilmentProcessor {
     this.fulfilmentToSendRepository = fulfilmentToSendRepository;
   }
 
+  @Transactional
   public void addFulfilmentBatchIdAndQuantity() {
-    List<FulfilmentCodeCount> fulfilmentsToSend =
-        fulfilmentToSendRepository.findCountOfFulfilments();
+    List<String> fulfilmentCodes = fulfilmentToSendRepository.findDistinctFulfilmentCode();
 
-    fulfilmentsToSend.forEach(
-        (fulfilment) -> {
+    fulfilmentCodes.forEach(
+        fulfilmentCode -> {
           jdbcTemplate.update(
-              "UPDATE actionv2.fulfilment_to_send SET quantity = ?, batch_id = ? "
-                  + "where fulfilment_code = ? ",
-              fulfilment.getCount(),
+              "UPDATE actionv2.fulfilment_to_send "
+                  + "SET quantity = (SELECT COUNT(*) FROM actionv2.fulfilment_to_send "
+                  + "WHERE fulfilment_code = ?), batch_id = ? WHERE fulfilment_code = ?",
+              fulfilmentCode,
               UUID.randomUUID(),
-              fulfilment.getFulfilmentCode());
+              fulfilmentCode);
         });
   }
 }
