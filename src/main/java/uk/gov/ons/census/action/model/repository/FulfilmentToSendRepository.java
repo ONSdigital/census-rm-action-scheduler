7005de09e9bc403b6db86a15d32145a8cd95fd1e
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-06-25 10:06:07 +0100

    Fix healthcheck thread starvation causing K8S pod restart (#96)

--- src/main/java/uk/gov/ons/census/action/model/repository/FulfilmentToSendRepository.java
+++ src/main/java/uk/gov/ons/census/action/model/repository/FulfilmentToSendRepository.java
@@ -3,10 +3,10 @@ package uk.gov.ons.census.action.model.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.gov.ons.census.action.model.entity.FulfilmentToProcess;
 
-@RestResource(exported = false)
+@RepositoryRestResource(exported = false)
 public interface FulfilmentToSendRepository extends JpaRepository<FulfilmentToProcess, Long> {
 
   @Query("SELECT DISTINCT f.fulfilmentCode FROM FulfilmentToProcess f")
